3223368428039362fc04f2bd59f67535aa53e0d1
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-09-23 00:10:08 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysqli_connect('youtube.cxtbv5qezsu9.us-east-2.rds.amazonaws.com', 'admin', 'sqladmin');
+$connection = mysqli_connect('projectdb.cxtbv5qezsu9.us-east-2.rds.amazonaws.com', 'admin', 'sqladmin');
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'mashlog_demo');
+$select_db = mysqli_select_db($connection, 'Project1');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 }
